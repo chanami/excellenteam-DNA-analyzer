f6ee09d1db5d1e1d4baac840357b9579df129cce
chanami <chanami15@gmail.com>
2018-12-28 03:19:09 -0800

    parser

--- parser.h
+++ parser.h
@@ -1,4 +1,9 @@
 #ifndef EXCELLENTEAM_ELLA_C_DNA_CHANAMI_PARSER_H
 #define EXCELLENTEAM_ELLA_C_DNA_CHANAMI_PARSER_H
 
+class Parser
+{
+public:
+    void parseString(int argc, char *argv[]);
+};
 #endif //EXCELLENTEAM_ELLA_C_DNA_CHANAMI_PARSER_H
