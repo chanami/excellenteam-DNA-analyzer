8502bbf7690d58bf6d3abd061780e3d40a3e9665
chanami <chanami15@gmail.com>
2018-12-29 12:52:00 -0800

    icommand

--- iCommand.h
+++ iCommand.h
@@ -1,4 +1,12 @@
 #ifndef EXCELLENTEAM_ELLA_C_DNA_CHANAMI_ICOMMAND_H
 #define EXCELLENTEAM_ELLA_C_DNA_CHANAMI_ICOMMAND_H
 
+class ICommand
+{
+public:
+    ICommand();
+    virtual ~ICommand();
+
+    virtual run();
+};
 #endif //EXCELLENTEAM_ELLA_C_DNA_CHANAMI_ICOMMAND_H
--- iDna.h
+++ iDna.h
@@ -4,12 +4,32 @@
 class IDna
 {
 public:
-    virtual IDna() = 0;
+    IDna() = 0;
     virtual ~IDna() = 0;
 
-    virtual size_t getDnaLength() = 0;
+    virtual size_t getDnaLength() const = 0;
+    char operator[](Dna &);
 
-    Neclutide operator[](Dna &);
 private:
+//    class Nucleotide
+//    {
+//    protected:
+//        void check_if_valid(char c_to_check) throw();
+//
+//    public:
+//        char c_sequence;
+//
+//        Nucleotide(char c ='A');
+//        bool operator == (char);
+//        Nucleotide* operator = (char);
+//        Nucleotide* operator = (Nucleotide);
+//        operator char()
+//        {
+//            return c_sequence;
+//        };
+//        char flip();
+//    };
+
 };
+
 #endif //EXCELLENTEAM_ELLA_C_DNA_CHANAMI_IDNA_H
